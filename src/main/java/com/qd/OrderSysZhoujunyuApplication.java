0959fb85a13a1d3788814ddc7493e131f70abb85
Jaylan-zhou <113752773+Jaylan-zhou@users.noreply.github.com>
2024-10-16 11:12:49 +0800

    项目初始化

---
+++ src/main/java/com/qd/OrderSysZhoujunyuApplication.java
@@ -0,0 +1,13 @@
+package com.qd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSysZhoujunyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSysZhoujunyuApplication.class, args);
+    }
+
+}
